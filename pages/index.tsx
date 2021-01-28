656a622dd6ca23c4ebdd44bae619c65b49c9b45b
Ronaldi Kurniawan Saphala <yeyrama@gmail.com>
2021-01-28 13:36:46 +0700

    Remove pulldown to refresh

--- pages/index.tsx
+++ pages/index.tsx
@@ -74,14 +74,6 @@ export default function Home() {
                   endMessage={repositories.length <= LIMIT ? undefined : <Alert closable className="text-center" message="All data has been fetched" type="success" />}
                   // below props only if you need pull down functionality
                   refreshFunction={loadMoreRows}
-                  pullDownToRefresh
-                  pullDownToRefreshThreshold={50}
-                  pullDownToRefreshContent={
-                    <h3 style={{ textAlign: 'center' }}>&#8595; Pull down to refresh</h3>
-                  }
-                  releaseToRefreshContent={
-                    <h3 style={{ textAlign: 'center' }}>&#8593; Release to refresh</h3>
-                  }
                 >
 
                   <List
